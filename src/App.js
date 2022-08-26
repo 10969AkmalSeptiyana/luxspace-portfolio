797e05c634dde8781c7a98e46bf8e81c52f9c7ba
AkmalSeptiyana <takaakmal10969@gmail.com>
2022-08-26 16:38:32 +0700

    create header and add jsconfig

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "pages/HomePage";
+
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
