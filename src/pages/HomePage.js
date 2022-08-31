8d5b1ba247d55deaf39f1e5fb97c1ac9e39cf1ae
AkmalSeptiyana <takaakmal10969@gmail.com>
2022-08-31 14:42:47 +0700

    create browse room and add helpers

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,19 @@
 import Header from "components/Header/Header";
+import BrowseRoom from "components/HomePage/BrowseRoom";
 import Hero from "components/HomePage/Hero";
 
 import useModalDOM from "helpers/hooks/useModalDOM";
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
 
 export default function HomePage() {
   useModalDOM();
+  useScrollAnchor();
+
   return (
     <>
       <Header theme="white" position="absolute" />
       <Hero />
+      <BrowseRoom />
     </>
   );
 }
